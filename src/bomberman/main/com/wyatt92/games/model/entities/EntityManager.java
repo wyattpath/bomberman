0b3b5a38e77de020dc1b8b238f07127f27c9efbe
viethoang92 <vi_hoang@hotmail.de>
2018-06-13 22:42:14 +0200

    Render Order

--- src/bomberman/main/com/wyatt92/games/model/entities/EntityManager.java
+++ src/bomberman/main/com/wyatt92/games/model/entities/EntityManager.java
@@ -4,12 +4,23 @@ import com.wyatt92.games.controller.Handler;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class EntityManager
 {
     private Handler handler;
     private Player player;
     private ArrayList<Entity> entities;
+    private Comparator<Entity> drawSorter = new Comparator<Entity>() {
+
+        @Override
+        public int compare(Entity a, Entity b)
+        {
+            if(a.getY() + a.getHeight() < b.getY() + b.getHeight())
+                return -1;
+            return 1;
+        }
+    };
 
     public EntityManager(Handler handler, Player player) {
         this.handler = handler;
@@ -23,6 +34,7 @@ public class EntityManager
             Entity e = entities.get(i);
             e.update();
         }
+        entities.sort(drawSorter);
     }
     public void draw(Graphics g){
         for(Entity e : entities){
