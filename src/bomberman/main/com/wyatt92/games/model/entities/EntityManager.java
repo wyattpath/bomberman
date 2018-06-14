8fcb1414061801b88add31bf7ce8b59447ed56d6
viethoang92 <vi_hoang@hotmail.de>
2018-06-14 22:10:08 +0200

    changed EntityManager loop to an iterator

--- src/bomberman/main/com/wyatt92/games/model/entities/EntityManager.java
+++ src/bomberman/main/com/wyatt92/games/model/entities/EntityManager.java
@@ -5,6 +5,7 @@ import com.wyatt92.games.controller.Handler;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Iterator;
 
 public class EntityManager
 {
@@ -30,11 +31,12 @@ public class EntityManager
     }
 
     public void update() {
-        for(int i = 0; i < entities.size(); i++){
-            Entity e = entities.get(i);
+        Iterator<Entity> it = entities.iterator();
+        while(it.hasNext()){
+            Entity e = it.next();
             e.update();
             if(!e.isActive())
-                entities.remove(e);
+                it.remove();
         }
         entities.sort(drawSorter);
     }
